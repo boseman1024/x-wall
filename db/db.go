7a25aa45c41cc54098fe431d7d793883d0d22512
boseman1024 <boseman1024@outlook.com>
2021-06-11 17:44:45 +0800

    v0.0.1

--- db/db.go
+++ db/db.go
@@ -11,19 +11,19 @@ import (
 
 var DB *sql.DB
 
-func Init(dbName string,user string,pwd string,host string,portStr string) (status bool){
-	if DB!=nil{
+func Init(dbName string, user string, pwd string, host string, portStr string) (status bool) {
+	if DB != nil {
 		DB.Close()
 	}
 	//"shares", "root", "123456", "localhost" 3306
-	port,_ := strconv.ParseInt(portStr,10,32)
+	port, _ := strconv.ParseInt(portStr, 10, 32)
 	db, err := manager.New(dbName, user, pwd, host).Set(
-			manager.SetCharset("utf8"),
-			manager.SetAllowCleartextPasswords(true),
-			manager.SetInterpolateParams(true),
-			manager.SetTimeout(1 * time.Second),
-			manager.SetReadTimeout(1 * time.Second),
-		).Port(int(port)).Open(true)
+		manager.SetCharset("utf8"),
+		manager.SetAllowCleartextPasswords(true),
+		manager.SetInterpolateParams(true),
+		manager.SetTimeout(1*time.Second),
+		manager.SetReadTimeout(1*time.Second),
+	).Port(int(port)).Open(true)
 
 	if err != nil {
 		fmt.Println("连接数据库失败")
@@ -33,8 +33,8 @@ func Init(dbName string,user string,pwd string,host string,portStr string) (stat
 	return true
 }
 
-func Disconnect() (status bool){
-	if DB!=nil{
+func Disconnect() (status bool) {
+	if DB != nil {
 		DB.Close()
 	}
 	return true
--- service/dbservice.go
+++ service/dbservice.go
@@ -1,16 +1,15 @@
 package service
 
 import (
-	"github.com/didi/gendry/scanner"
 	"x-wall/db"
 )
 
 type DbConnectService struct {
-	Name string `json:"name"`
-	Username   string `json:"username"`
-	Password    string `json:"password"`
-	Host   string `json:"host"`
-	Port   string `json:"port"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Host     string `json:"host"`
+	Port     string `json:"port"`
 }
 
 type DbService struct {
@@ -18,7 +17,7 @@ type DbService struct {
 }
 
 func (dbConnectService *DbConnectService) Connect() (status bool) {
-	status = db.Init(dbConnectService.Name,dbConnectService.Username,dbConnectService.Password,dbConnectService.Host,dbConnectService.Port)
+	status = db.Init(dbConnectService.Name, dbConnectService.Username, dbConnectService.Password, dbConnectService.Host, dbConnectService.Port)
 	return status
 }
 
@@ -27,8 +26,35 @@ func (dbConnectService *DbConnectService) Disconnect() (status bool) {
 	return status
 }
 
-func (dbService *DbService) ExecSql() (result []map[string]interface{},err error) {
-	rows,err := db.DB.Query(dbService.SqlText)
-	result,err = scanner.ScanMapClose(rows)
-	return result,err
+func (dbService *DbService) ExecSql() (results []map[string]string, err error) {
+	rows, err := db.DB.Query(dbService.SqlText)
+	if err != nil {
+		return nil, err
+	}
+	//关闭rows链接
+	defer rows.Close()
+	//读出查询出的列字段名
+	cols, _ := rows.Columns()
+	//values是每个列的值，这里获取到byte里
+	values := make([][]byte, len(cols))
+	//query.Scan的参数，因为每次查询出来的列是不定长的，用len(cols)定住当次查询的长度
+	scans := make([]interface{}, len(cols))
+	//让每一行数据都填充到[][]byte里面,狸猫换太子
+	for i := range values {
+		scans[i] = &values[i]
+	}
+	results = make([]map[string]string, 0, 10)
+	for rows.Next() {
+		err := rows.Scan(scans...)
+		if err != nil {
+			return nil, err
+		}
+		row := make(map[string]string, 10)
+		for k, v := range values { //每行数据是放在values里面，现在把它挪到row里
+			key := cols[k]
+			row[key] = string(v)
+		}
+		results = append(results, row)
+	}
+	return results, nil
 }
